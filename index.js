b0927081dbbe5393976773e0c67ca014c5a3136d
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-12-05 23:12:49 +0600

    server side setup

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middlewear
+
+app.use(cors())
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kmvb6d0.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+}
+run().catch(console.log)
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal server is running')
+
+})
+
+
+app.listen(port, () => {
+    console.log(`doctors portal running ${port}`)
+})
+
+

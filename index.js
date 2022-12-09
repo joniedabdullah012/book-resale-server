567fb7e0efb22238767b22207b92beb4a62c89d6
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-12-10 03:00:39 +0600

    catagoryId/:id bookCatagories

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
+// const details = require('./data.json');
 
 
 
@@ -21,8 +22,79 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
 
+    try {
+        const bookCategories = client.db('bookResale').collection('bookCatagories')
+        app.get('/bookCatagories', async (req, res) => {
+            const query = {};
+            const catagories = await bookCategories.find(query).toArray();
+            res.send(catagories)
+
+
+        });
+
+        const bookCatagoriesId = client.db('bookResale').collection('bookCatagoriesId')
+        app.get('/bookCatagoriesId', async (req, res) => {
+
+            const bookId = {}
+
+            const catagory = await bookCatagoriesId.find(bookId).toArray()
+            res.send(catagory)
+
+        })
+        const bookCatagoriesIdnew = client.db('bookResale').collection('bookCatagoriesId')
+        app.get('/bookCatagoriesId/:id', async (req, res) => {
+            console.log(req.params.id);
+
+            const id = req.params.id;
+            const query = { category: (id) };
+
+
+            const catagory = await bookCatagoriesIdnew.find(query).toArray()
+            res.send(catagory)
+
+        })
+
+
+        // const bookCatagoriesId = client.db('bookResale').collection('bookCatagoriesId')
+        // app.get('/bookCatagoriesId/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const bookId = { _id: ObjectId(id) };
+
+
+        //     const catagory = await bookCatagoriesId.findOne(bookId)
+        //     res.send(catagory)
+
+        // })
+
+
+
+    }
+
+    finally {
+
+    }
+
 }
 run().catch(console.log)
+
+
+// practice 
+
+app.get('/cataegory/:id', async (req, res) => {
+    const id = req.params.id;
+    const catagory = await details.filter(n => n.category_id === id);
+    res.send(catagory)
+
+
+
+})
+app.get('/cataegory', async (req, res) => {
+
+    res.send(details)
+
+
+
+})
 
 
 
